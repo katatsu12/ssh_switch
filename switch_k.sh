e82ccf5fcca620b7f1425d96e1216b46e0e8e57e
den <katatsu12@gmail.com>
2019-10-22 22:07:25 +0300

    Update script
    Signed-off-by: den <katatsu12@gmail.com>

--- switch_k.sh
+++ switch_k.sh
@@ -1,6 +1,19 @@
-dirs=( $(ls -1p ~/.ssh/ | grep profile) )
+menu_from_array ()
+{
 
-select dir in "${dirs[@]}"
-do
-  cp -r ~/.ssh/$dir* ~/.ssh/
-done
+  select item; do
+    if [ 1 -le "$REPLY" ] && [ "$REPLY" -le $# ];
+
+    then
+      echo "Done. Current: $item"
+      cp -r ~/.ssh/$item* ~/.ssh/
+      break;
+    else
+      echo "Wrong selection: Select profile from 1-$#"
+    fi
+  done
+}
+
+profiles=($(ls -1p ~/.ssh/ | grep profile))
+
+menu_from_array "${profiles[@]}"
